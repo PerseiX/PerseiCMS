d2205890c545ff52483be0825917b9c42b1f46d4
PerseiX <kamilrytel2@gmail.com>
2016-02-11 19:32:04 +0100

    Geneate bundle, remove default bundle

--- src/Cms/UserBundle/CmsUserBundle.php
+++ src/Cms/UserBundle/CmsUserBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace Cms\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class CmsUserBundle extends Bundle
 {
 }
